64fc13dd78903d91cec92353adb09079b368a495
vusdlq007 <vusdlq007@naver.com>
2022-09-24 18:53:20 +0900

    init

---
+++ alphatrip-Core-Api/src/main/java/com/alphatrip/alphatripcoreapi/AlphatripCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.alphatrip.alphatripcoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphatripCoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphatripCoreApiApplication.class, args);
+    }
+
+}
---
+++ alphatrip-Core-Api/src/test/java/com/alphatrip/alphatripcoreapi/AlphatripCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphatrip.alphatripcoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphatripCoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
